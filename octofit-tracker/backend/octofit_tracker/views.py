72e6cf00d5d34ccb949118745dd00ef9c20c27a8
Guillermo Correa <guillermo.correa.99@gmail.com>
2025-04-08 04:03:57 +0000

    step 4

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -9,7 +9,7 @@ def api_root(request, format=None):
     if request.method == 'POST':
         return Response({"message": "POST request received"}, status=status.HTTP_201_CREATED)
 
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://super-space-lamp-65v6q7j9w3r475-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/?format=api',
         'teams': base_url + 'api/teams/?format=api',
